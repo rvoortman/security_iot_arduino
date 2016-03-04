087fd101a94ec1077c203bbe4d86865120cbb48a
Rob <robgerritsen11@gmail.com>
2016-03-04 20:20:37 +0100

    FEATURE: add NRF24 code. > send code to server > wait max X ms for response > no response = resend code, Max resends 5 > expected response 0/1
    10 good codes and 10 bad codes are cached.
    if code is in cache then nothing will be send to the server
    after 10 codes the first 1 will be overridden

---
+++ src/NRF24.ino
@@ -0,0 +1,162 @@
+
+
+#include <SPI.h>
+#include "RF24.h"
+#include "printf.h"
+
+// The various roles supported by this sketch
+typedef enum { role_sender = 1, role_receiver } role_e;
+
+// The debug-friendly names of those roles debug things
+const char* role_friendly_name[] = { "invalid", "Sender", "Receiver"};
+role_e role;
+const short role_pin = 3;
+
+//
+// settings
+//
+RF24 radio(9,10);
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };   // Radio pipe addresses for the 2 nodes to communicate.
+uint8_t MAX_ATTEMPTS = 5;
+uint16_t TIMEOUT_MS = 500;
+
+
+// setup radio
+void initNRF(){
+  pinMode(role_pin, INPUT);
+  digitalWrite(role_pin,HIGH);
+
+  radio.begin();                           // Setup and configure rf radio
+  radio.setRetries(15,15);
+  // optionally, reduce the payload size.  seems to
+  // improve reliability
+  radio.setPayloadSize(8);
+
+  // read the address pin, establish our role
+  if (digitalRead(role_pin) ) {
+    role = role_sender;
+  }  else {
+    role = role_receiver;
+  }
+  // set pipes
+  if ( role == role_sender )
+  {
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);
+  } else {
+    radio.openWritingPipe(pipes[1]);
+    radio.openReadingPipe(1,pipes[0]);
+  }
+
+  printf("ROLE: %s\n\r",role_friendly_name[role]);
+  radio.startListening();                 // Start listening
+  radio.printDetails();
+}
+
+// keep sending a test code
+void testRadio(){
+  if ( role == role_sender ) {
+    uint16_t x = 1000;
+    sendCode(x);
+
+    delay(5000);
+  } else {
+    readCode();
+  }
+}
+
+
+/**
+* Send code to server
+**/
+void sendCode(uint16_t code) {
+
+  bool timeout = true;
+  uint8_t attempt = 1;
+  // run once, more if server does not send a response
+  do {
+  // First, stop listening so we can talk.
+  radio.stopListening();
+  printf("Now sending %i...",code);
+  radio.write( &code, sizeof(uint16_t) );
+
+  // Now, continue listening
+  radio.startListening();
+  // give server some time to respond
+  timeout = waitForResponse();
+  // Describe the results
+  if (!timeout )
+  {
+    // Grab the response, compare, and send to debugging spew
+    uint16_t got_result;
+    radio.read( &got_result, sizeof(uint16_t) );
+    // Spew it
+    printf("Got response %i\n\r",got_result);
+    handleResponse(got_result, code);
+    // we are done
+    return;
+  }
+  printf("Failed, response timed out.\n\r");
+  attempt++;
+} while(timeout && attempt <= MAX_ATTEMPTS);
+c
+// we tried X times but no response;
+  handleFail();
+}
+
+
+// wait for the server to send a response.
+// this functions block for max @TIMEOUT_MS
+bool waitForResponse(){
+  // Wait here until we get a response, or timeout (250ms)
+  unsigned long started_waiting_at = millis();
+  bool timeout = false;
+  while ( ! radio.available() && ! timeout ) {
+    if (millis() - started_waiting_at > TIMEOUT_MS ){
+      timeout = true;
+    }
+  }
+  return timeout;
+
+}
+
+// some code for making a receiver arduino
+void readCode() {
+  // if there is data ready
+  if (radio.available())
+  {
+    digitalWrite(LED_R, LOW);
+    printf("read\n");
+    // Dump the payloads until we've gotten everything
+    uint16_t got_code;
+    bool done = false;
+    while (!done)
+    {
+      // Fetch the payload, and see if this was the last one.
+      done = radio.read( &got_code, sizeof(uint16_t) );
+
+      // Spew it
+      printf("Got payload %i...",got_code);
+
+      // Delay just a little bit to let the other unit
+      // make the transition to receiver
+      delay(20);
+    }
+
+    // First, stop listening so we can talk
+    radio.stopListening();
+
+    // Send the final one back.
+    if(got_code == 1000){
+      uint16_t x = 1;
+    } else {
+      uint16_t x = 0;
+    }
+    radio.write( &x, sizeof(uint16_t) );
+    printf("Sent response.\n\r");
+    // Now, resume listening so we catch the next packets.
+    radio.startListening();
+  } else {
+    digitalWrite(LED_R, HIGH);
+  }
+}
--- src/security.ino
+++ src/security.ino
@@ -1,8 +1,21 @@
 
+
+#include "printf.h"
+uint8_t MAX_FAILS = 3;
+uint8_t fails = 0;
+uint8_t index_good = 0;
+uint8_t index_bad = 0;
+uint8_t ARRAY_SIZE = 10;
+uint16_t good[10] = {0,0,0,0,0,0,0,0,0,0};
+uint16_t bad[10] = {0,0,0,0,0,0,0,0,0,0};
 /**
 * Code run on startup
 */
 void setup(){
+
+    Serial.begin(115200);
+    printf_begin();
+    initNRF();
 
 }
 
@@ -10,5 +23,89 @@ void setup(){
 * Code run continuously
 */
 void loop(){
+  //testRadio();
 
+}
+
+void openDoor(uint16_t code){
+  if(preCheck(code)){
+    return; // door is open | alarm is raised;
+  }
+  // send code to server to see if user can open this door.
+  sendCode(code);
+}
+
+
+
+/**
+* inital check for valit code and cache
+* return true if preCheck was succesful, meaning no communication with the server is needed.
+**/
+bool preCheck(uint16_t code){
+  // checkCode for valit input
+  if(!checkCode(code)){
+    handleFail();
+    return true;
+  }
+  // TODO build cache. Is there a timeout for a cached value?
+  for(uint8_t x = 0; x < ARRAY_SIZE;x++){
+    //  printf("%i\n", x);
+      if(good[x] == code){
+        openDoor();
+        return true;
+      }
+      if(bad[x] == code){
+        handleFail();
+        return true;
+      }
+  }
+  return false;
+
+}
+// check for valit input
+bool checkCode(uint16_t code) {
+  return (code >= 1000 && code <=9999);
+}
+
+// add to good cache
+void addToGoodCache(uint16_t value){
+  if(index_good == ARRAY_SIZE){
+      index_good = 0;
+  }
+  good[index_good] = value;
+  index_good++;
+}
+// add to bad cache
+void addToBadCache(uint16_t value){
+  if(index_bad == ARRAY_SIZE){
+      index_bad = 0;
+  }
+  bad[index_bad] = value;
+  index_bad++;
+}
+
+// handle a response from NRF24, result + code entert
+void handleResponse(uint16_t result, uint16_t code){
+  if(result == 1){ // succes add code to cache
+    addToGoodCache(code);
+    openDoor();
+    return;
+  } // fail add fail to bad code cache
+  addToBadCache(code);
+  handleFail();
+
+}
+
+void handleFail(){
+  fails++;
+  if(fails >= MAX_FAILS){
+    // TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! alarm
+  }
+}
+
+/** Open the door code accepted.
+*/
+void openDoor(){
+  fails = 0; // reset fails.
+  // TODO open the door
 }
